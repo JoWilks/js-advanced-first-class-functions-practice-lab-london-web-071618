74cda6239cecf6fe7fd21ac9d3ff85e4d60e326c
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-29 19:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function driversByRevenue(drivers) {
   
 function driversByName(drivers) {
      return [...drivers].sort(function(a,b) {
-     return a.revenue - b.revenue
+     return a.name - b.name
   })
   }
   
